cc2e9ac6ea8071720368aa0eb00199918d183d4d
Allison Tearjen <amtearjen@ucdavis.edu>
2018-09-09 21:29:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,7 +26,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board,index) == true
-    move(board,index,token = 'X')
+    player_move(board,index,token = 'X')
     display_board(board)
   else
     turn(board)
